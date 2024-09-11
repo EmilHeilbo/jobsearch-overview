476523bec360632fc907d8914065714b5f693a22
Emil Heilbo <ehe@duka.dk>
2024-09-11 09:40:19 +0200

    chore: add favicon, title, more placeholders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	let testVar = "Dynamic option";
+	let testArr = ["Array", "Options", "Here"];
+</script>
+
 <header>
 	<h1>Welcome to SvelteKit</h1>
 </header>
@@ -7,9 +12,32 @@
 </main>
 
 <aside>
-	<p>This is in the sidebar</p>
+	<section>
+		<h1>This is in the sidebar</h1>
+		<label>
+			Big label
+			<small>Small label</small>
+			<select>
+				<option>Static option</option>
+				<option>{testVar}</option>
+				{#each testArr as _}
+					<option>{_}</option>
+				{/each}
+			</select>
+		</label>
+	</section>
 </aside>
 
 <footer>
-	<p>&COPY; Emil Heilbo — 2024</p>
+	<p>&copy;2024 – Emil Heilbo</p>
+	<small>
+		Made with <a href="https://kit.svelte.dev">SvelteKit</a>, <a href="https://prisma.io">Prisma</a> & <a href="https://matcha.mizu.sh">matcha.css</a>
+	</small>
 </footer>
+
+
+<style>
+	a {
+		color: #8ff0a4;
+	}
+</style>
