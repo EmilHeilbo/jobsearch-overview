2602a1ad982bfea4a59666375fddfd7ac0f2a884
Emil Heilbo <19676610+EmilHeilbo@users.noreply.github.com>
2024-09-25 13:04:05 +0200

    feat: add link buttons to job search sites

---
+++ src/lib/components/LinkButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	function capitalizeLink(link: string) {
+		if (!link) return '';
+		let _link = link.slice(8).split('.')[0];
+		return _link.slice(0, 1).toUpperCase() + _link.slice(1);
+	}
+	function getFavicon(link: string) {
+		if (!link) return '';
+		return `https://${link.slice(8).split('/')[0]}/favicon.ico`
+	}
+	export let address: string;
+</script>
+
+<button on:click={() => window.open(address)}>
+	<img src={getFavicon(address)} alt="icon" />
+	{capitalizeLink(address)}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Job from '$lib/components/Job.svelte';
+	import LinkButton from '$lib/components/LinkButton.svelte';
 	import type { PageData } from './$types';
 	let dialog: { showModal: () => any };
 	export let data: PageData;
@@ -8,8 +9,11 @@
 <aside>
 	<section>
 		<h1>This is in the sidebar</h1>
-		<label>🧑‍💻 Job Applicant
-			<select>
+		<h2>🔍Job Search Sites</h2>
+		<LinkButton address="https://jobindex.dk/jobsoegning" />
+		<LinkButton address="https://linkedin.com/jobs" />
+		<label
+			>🧑‍💻 Job Applicant
 			<select id="userSelect">
 				{#each data.users as _}
 					<option value={_.id}>{_.name}</option>
