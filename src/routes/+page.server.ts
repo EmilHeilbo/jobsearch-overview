a016e5db2aed1debb01e236c074af6cca255126f
Emil Heilbo <19676610+EmilHeilbo@users.noreply.github.com>
2024-09-27 13:12:03 +0200

    feat: auto-check if job has distance and fetch from GraphHopper

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,35 @@
 import prisma from '$lib/prisma';
 import type { PageServerLoad } from './$types';
+import { getCoordinates, getRoute } from '$lib/graphhopper';
 
 export const load = (async () => {
+	await prisma.job.findMany(
+		{
+			where: { distance: null },
+			include: { applicant: true },
+		}
+	).then((jobs) => jobs.forEach(async (job) => {
+		if (!isNaN(Number(job.distance)) && Number(job.distance)) {
+			console.log(`SKIP: job ${job.id} - ${job.location} - ${job.distance} km`);
+			return;
+		}
+		console.log(`CALC: ${job.id} - ${job.location} - ${job.distance} km`);
+		let route = await getRoute([
+			await getCoordinates(job.applicant.location),
+			await getCoordinates(job.location)
+		]);
+		await prisma.job.update({
+			where: {
+				id: job.id
+			},
+			data: {
+				distance: route.kilometers,
+				travelTime: route.minutes
+			}
+		});
+	}));
 	const users = await prisma.user.findMany();
 	const jobs = await prisma.job.findMany({ include: { applicant: true } });
 
-return { jobs: jobs, users: users };
+	return { jobs: jobs, users: users };
 }) satisfies PageServerLoad;
